f15b08ad89e066781d676f02ab16b06cf5faba4d
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-10-04 21:57:01 +0200

    .....

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $products      = Product::all();
+        $products = Product::get();
 
         return view('website.home' , compact('products'));
     }
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -65,33 +65,83 @@
     </div>
 </div>
 
-<div class="untree_co-section">
+{{-- <div class="untree_co-section">
     <div class="container">
-
         <div class="row">
             <div class="col-6 col-sm-6 col-md-6 mb-4 col-lg-4">
                 @forelse($products as $product )
-                    <div class="product-item">
-                        <a href="shop-single.html" class="product-img">
-
-                            <div class="label new top-right">
-                                <div class='content'>New</div>
+                    <div class="item">
+                        <div class="product-item">
+                            <a href="javascript:void(0);" class="product-img">
+                                <div class="label new top-right">
+                                    <div class='content'>New</div>
+                                </div>
+                                <img src="{{ $product->image_name }}" alt="Image" class="img-fluid">
+                            </a>
+                            <h3 class="title"><a href="javascript:void(0);">{{ $product->name }}</a></h3>
+                            <div class="price">
+                                <span>{{ $product->pricessss ?? 'no price yet' }} EGP</span>
                             </div>
-                            <img src="{{ $product->image_name }}" alt="Image" class="img-fluid">
-                        </a>
-                        <h3 class="title"><a href="javascript:void(0);">{{ $product->name }}</a></h3>
-                        <div class="price">
-                            <span>{{ $product->prices ?? 'no price yet' }} EGP</span>
                         </div>
                     </div>
                 @empty
-
+                    noooooooooooo data!!!!!!!!!!!!!!!!!!
                 @endforelse
-
-
             </div>
         </div>
     </div>
+</div> <!-- /.untree_co-section --> --}}
+
+<div class="untree_co-section">
+    <div class="container">
+        <div class="row mb-5 align-items-center">
+            <div class="col-md-6">
+                <h2 class="h3">Popular Items</h2>
+            </div>
+            <div class="col-sm-6 carousel-nav text-sm-right">
+                <a href="javascript:void(0);" class="prev js-custom-prev-v2">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path fill-rule="evenodd" d="M8.354 11.354a.5.5 0 0 0 0-.708L5.707 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z"/>
+                        <path fill-rule="evenodd" d="M11.5 8a.5.5 0 0 0-.5-.5H6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5z"/>
+                    </svg>
+                </a>
+                <a href="javascript:void(0);" class="next js-custom-next-v2">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path fill-rule="evenodd" d="M7.646 11.354a.5.5 0 0 1 0-.708L10.293 8 7.646 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"/>
+                        <path fill-rule="evenodd" d="M4.5 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </a>
+            </div>
+        </div> <!-- /.heading -->
+        <div class="owl-3-slider owl-carousel">
+    
+            @forelse($products as $product )
+            <div class="item">
+                <div class="product-item">
+                    <a href="shop-single.html" class="product-img">
+                        <div class="label new top-right">
+                            <div class='content'>New</div>
+                        </div>
+
+                        <div class="label sale top-right second">
+                            <div class='content'>Sale</div>
+                        </div>
+                        <img src="{{ $product->image_name}}" alt="Image" class="img-fluid">
+                    </a>
+                    <h3 class="title"><a href="javascript:void(0);">{{ $product->name }}</a></h3>
+                    <div class="price">
+                        <del>{{ $product->price }} EGP</del> &mdash; <span>{{ $product->price - ($product->price * 0.10) }} EGP</span>
+                    </div>
+                </div>
+            </div> <!-- /.item -->
+            @empty
+                noooo dataaa!!!!
+            @endforelse
+
+        </div>
+    </div> <!-- /.container -->
 </div> <!-- /.untree_co-section -->
 
 
