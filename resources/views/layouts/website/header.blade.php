10c9bb4c4a85e5f9267441cad9275e866b3b3cc1
nada5694 <96081397+nada5694@users.noreply.github.com>
2022-10-10 08:34:28 +0300

    sdgas

--- resources/views/layouts/website/header.blade.php
+++ resources/views/layouts/website/header.blade.php
@@ -99,15 +99,25 @@
                         </svg>
                     </a>
 
-<<<<<<< HEAD
-                    <a href="javascript:void(0);">
-                        <svg class="Setting" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-=======
-                    <a href="javascript:void(0);" class="three-dots">
-                        <svg class="bi bi-Setting" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
->>>>>>> c76b46ca26cfdafa971ae626ff0a15749fc35106
-                            <path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z"/></svg>
-                    </a>
+                    <div class="dropdown show" style="display: inline;">
+                        <a class="btn three-dots" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <svg class="bi bi-Setting" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                                <path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z"/></svg>
+
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        @if(!auth()->user())
+                          <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                          <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                        @elseif(auth()->user())
+                            <a class="ml-2" href="{{ route('logout') }}" onclick="event.preventDefault(); document.querySelector('#logout-form').submit();"><i class="fa-solid fa-arrow-right-from-bracket"></i><span>Logout</span></a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        @endif
+                        </div>
+                      </div>
                     {{-- <ul class="dropdown">
                         <li><a href="{{ route('login') }}">Login</a></li>
                         <li><a href="{{ route('register') }}">Register</a></li>
