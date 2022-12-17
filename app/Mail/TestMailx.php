8ee5958a1ee1dd3aed879c65f2a30317539d50b4
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-12-18 01:37:40 +0200

    ....

--- app/Mail/TestMailx.php
+++ app/Mail/TestMailx.php
@@ -32,7 +32,7 @@ class TestMail extends Mailable
     public function envelope()
     {
         return new Envelope(
-            from: new Address('UNTREESTORE@company.com', 'Laravel'),
+            from: new Address('', 'Laravel'),
             subject: 'Test Mail',
         );
     }
